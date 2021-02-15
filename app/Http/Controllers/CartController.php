34c8b81945d348ee8af581a98935134ef45d1f02
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-02-15 10:56:12 +0900

    UC

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -59,6 +59,18 @@ class CartController extends Controller
     public function confirm(Request $request)
     {
         $data = [];
+        if ($request->session()->has('user_items')) {
+            $user_items = UserItem::sessionValues($request);
+
+            //SELECT * FROM items WHERE id IN (xx, xx, xx);
+            $items = Item::whereIn('id', array_keys($user_items))->get();
+            $total_price = UserItem::calculateTotal($request);
+            $data = [
+                'user_items' => $user_items,
+                'items' => $items,
+                'total_price' => $total_price,
+            ];
+        }
         return view('cart.confirm', $data);
     }
 
