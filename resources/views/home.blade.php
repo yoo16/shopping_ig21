c3fdbef8e9dd940d81c4f0944fe60fa6361d0bf9
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-02-17 10:33:57 +0900

    UC

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,23 +5,7 @@
 
   <div class="row">
 
-    @if (isset($items))
-    @foreach ($items as $item)
-    <div class="col-md-4 mb-2">
-
-      <p>
-        {{ $item->name }}
-      </p>
-      <p class="row justify-content-center">
-        <img src="{{ asset('images/now_printing.jpg') }}" alt="" width="200">
-      </p>
-
-      <p class="row justify-content-center">
-        <a href="{{ route('cart.add', ['id' => $item->id]) }}" class="btn btn-outline-primary">カートに入れる</a>
-      </p>
-    </div>
-    @endforeach
-    @endif
+ 
 
   </div>
 </div>
